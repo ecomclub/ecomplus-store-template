19cf0c2fe2d028d161bbeb39510ff556f0d89592
Leonardo Matos <leomp120894@gmail.com>
2018-03-06 13:40:22 -0300

    update v1, render v1.6.2

--- src/01-Concat.php
+++ src/01-Concat.php
@@ -6,7 +6,7 @@ $files = array(
   // Vue 2
   'https://cdn.jsdelivr.net/npm/vue@2/dist/vue.min.js',
   // storefront SDK and render in current version
-  'https://cdn.jsdelivr.net/npm/ecomplus-sdk@1/dist/sdk.min.js',
+  'https://cdn.jsdelivr.net/npm/ecomplus-sdk@1.6.2/dist/sdk.min.js',
   'https://cdn.jsdelivr.net/npm/ecomplus-render@1.8.6/dist/render.min.js'
 );
 
