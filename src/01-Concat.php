7ea6d183b70a15cec05c68aa4572bf8c2ec72092
Leonardo Matos <leomp120894@gmail.com>
2018-11-20 14:24:41 -0200

    update v2, render v2.9.2

--- src/01-Concat.php
+++ src/01-Concat.php
@@ -7,7 +7,7 @@ $files = array(
   'https://cdn.jsdelivr.net/npm/vue@2/dist/vue.min.js',
   // storefront SDK and render in current version
   'https://cdn.jsdelivr.net/npm/ecomplus-sdk@1.15.2/dist/sdk.min.js',
-  'https://cdn.jsdelivr.net/npm/ecomplus-render@2.9.1/dist/render.min.js'
+  'https://cdn.jsdelivr.net/npm/ecomplus-render@2.9.2/dist/render.min.js'
 );
 
 // start with polyfill script
